9071f893aa861a098facf21a0f515d518e571644
Homo <Homo@114514.com>
2023-03-23 09:47:18 +0800

    update

--- Delegator/Delegator.go
+++ Delegator/Delegator.go
@@ -11,49 +11,52 @@ import (
 const (
 	Normal = iota
 	Cycle
-	max
+	max //占位
 	unEffective
 )
 
 type Delegator interface {
-	Cores         //核心功能
-	Concurrency   //异步委托
-	TimeOperation //时间操作
-	CarryPattern  //执行模式
+	cores         //核心功能
+	concurrency   //异步委托
+	timeOperation //时间操作
+	carryPattern  //执行模式
 }
 
 //// Delegatorβ 支持并发的委托接口
 //type Delegatorβ interface {
-//	Concurrency //并发功能
+//	concurrency //并发功能
 //	Delegator      //原委托接口的功能
 //}
 
-// Cores 委托核心功能接口
-type Cores interface {
+// cores 委托核心功能接口
+type cores interface {
 	Load(f interface{}, args ...interface{}) Delegator
+	Quick(fs ...interface{}) Delegator
+	Same(f interface{}, num int, args ...interface{}) Delegator
 	Join(delegator2 Delegator) Delegator
-	Run(params ...interface{})
+	Run(params ...interface{}) error
 	Genshin()
 	ShowMembers(pattern int)
 	GetReturns() Returner
 	back() *delegator
 }
 
-// Concurrency 异步接口
-type Concurrency interface {
+// concurrency 异步接口
+type concurrency interface {
 	Stop()
 	Start()
 	Wait()
 	Over()
+	Sleep(duration time.Duration)
 }
 
-// TimeOperation 时间操作的接口
-type TimeOperation interface {
+// timeOperation 时间操作的接口
+type timeOperation interface {
 	SetTime(t time.Duration, index int) Delegator
 }
 
-// CarryPattern 执行模式的接口
-type CarryPattern interface {
+// carryPattern 执行模式的接口
+type carryPattern interface {
 	SetPattern(pattern int, args ...interface{}) Delegator
 }
 
@@ -65,15 +68,20 @@ type Returner interface {
 // 委托实体
 type delegator struct {
 	fs      []func() []reflect.Value //函数队列
-	stop    chan int                 //无缓存管道,用来阻塞委托
-	start   chan int                 //无缓存管道,用来恢复委托
-	signal  chan int                 //委托完成的信号
-	over    chan int                 //终止委托的信号
-	returns chan returner            //存储返回值的管道
+	cs      *chans                   //管道集合
 	names   []string                 //记录函数名
 	sleeper []time.Duration          //目标函数需要睡眠的时间
 	ptn     *pattern                 //执行模式
+	err     error                    //记录错误,在委托run的时候返回
 	typ     bool                     //判断是否为异步委托
+}
+
+type chans struct {
+	stop    chan int      //无缓存管道,用来阻塞委托
+	start   chan int      //无缓存管道,用来恢复委托
+	signal  chan int      //委托完成的信号
+	over    chan int      //终止委托的信号
+	returns chan returner //存储返回值的管道
 }
 
 type pattern struct {
@@ -89,39 +97,113 @@ type returner struct {
 // New 根据参数生成不同运行模式的委托实体
 func New(args ...interface{}) Delegator {
 	if len(args) == 0 {
-		return &delegator{fs: make([]func() []reflect.Value, 0), names: make([]string, 0), stop: make(chan int), signal: make(chan int), over: make(chan int), start: make(chan int), returns: make(chan returner, 1), typ: false, ptn: &pattern{
-			carryPattern: 0, //默认是0
-			args:         make([]interface{}, 0),
-		}}
+		return &delegator{
+			fs:    make([]func() []reflect.Value, 0),
+			names: make([]string, 0),
+			cs: &chans{
+				stop:    make(chan int),
+				start:   make(chan int),
+				signal:  make(chan int),
+				over:    make(chan int),
+				returns: make(chan returner, 1),
+			},
+			typ: false,
+			ptn: &pattern{
+				carryPattern: 0, //默认是0
+				args:         make([]interface{}, 0),
+			},
+		}
 	} else {
-		return &delegator{fs: make([]func() []reflect.Value, 0), names: make([]string, 0), stop: make(chan int), signal: make(chan int), over: make(chan int), start: make(chan int), returns: make(chan returner, 1), typ: true, ptn: &pattern{
-			carryPattern: 0, //默认是0
-			args:         make([]interface{}, 0),
-		}}
+		return &delegator{
+			fs:    make([]func() []reflect.Value, 0),
+			names: make([]string, 0),
+			cs: &chans{
+				stop:    make(chan int),
+				start:   make(chan int),
+				signal:  make(chan int),
+				over:    make(chan int),
+				returns: make(chan returner, 1),
+			},
+			typ: true,
+			ptn: &pattern{
+				carryPattern: 0, //默认是0
+				args:         make([]interface{}, 0),
+			},
+		}
 	}
+}
+
+// 委托的装载内核,妈的,要适配Same方法所以抽取出来了
+func (d *delegator) load(f interface{}, args interface{}) Delegator {
+	ars, ok := args.([]interface{})
+	if ok {
+		//先获取函数的类型反射对象
+		t := reflect.TypeOf(f)
+		v := reflect.ValueOf(f)
+		fName := runtime.FuncForPC(v.Pointer()).Name()
+		//获取函数类型反射对象的入参数
+		inParams := make([]reflect.Value, t.NumIn())
+
+		for i := 0; i < t.NumIn(); i++ {
+			if i > len(ars)-1 {
+				//不好好传参,就只能在这里给你们进行默认值处理,妈的,只处理了一些常见的类型
+				switch t.In(i).Kind() {
+				case reflect.Int:
+					inParams[i] = reflect.ValueOf(0)
+				case reflect.Float64, reflect.Float32:
+					inParams[i] = reflect.ValueOf(0.0)
+				case reflect.String:
+					inParams[i] = reflect.ValueOf("")
+				case reflect.Bool:
+					inParams[i] = reflect.ValueOf(false)
+				default:
+					d.err = fmt.Errorf("error ! uncorrect in params , please check")
+				}
+			} else {
+				inParams[i] = reflect.ValueOf(ars[i])
+			}
+		}
+
+		//闭包
+		d.fs = append(d.fs, func() []reflect.Value { return v.Call(inParams) })
+		d.sleeper = append(d.sleeper, 0)
+
+		tmp := v.String()[1:]
+		tmp = tmp[:len(tmp)-6]
+		formatName := fmt.Sprintf("函数名:%s,签名:%s\n", fName, tmp)
+		d.names = append(d.names, formatName)
+	}
+	return d
 }
 
 // Load 为委托装载函数,f参数为目标函数,args为可选参数,按顺序识别,多于目标函数入参的参数无效.
 func (d *delegator) Load(f interface{}, args ...interface{}) Delegator {
-	//先获取函数的类型反射对象
-	t := reflect.TypeOf(f)
-	v := reflect.ValueOf(f)
-	fName := runtime.FuncForPC(v.Pointer()).Name()
-	//获取函数类型反射对象的入参数
-	inParams := make([]reflect.Value, t.NumIn())
-
-	for i := 0; i < t.NumIn(); i++ {
-		inParams[i] = reflect.ValueOf(args[i])
+	if d.err != nil {
+		return d
 	}
+	d.load(f, args)
+	return d
+}
 
-	//闭包
-	d.fs = append(d.fs, func() []reflect.Value { return v.Call(inParams) })
-	d.sleeper = append(d.sleeper, 0)
+// Quick 快速装填多个无入参的函数
+func (d *delegator) Quick(fs ...interface{}) Delegator {
+	if d.err != nil {
+		return d
+	}
+	for _, f := range fs {
+		d.Load(f)
+	}
+	return d
+}
 
-	tmp := v.String()[1:]
-	tmp = tmp[:len(tmp)-6]
-	formatName := fmt.Sprintf("函数名:%s,签名:%s\n", fName, tmp)
-	d.names = append(d.names, formatName)
+// Same 装填多个相同的函数
+func (d *delegator) Same(f interface{}, num int, args ...interface{}) Delegator {
+	if d.err != nil {
+		return d
+	}
+	for i := 0; i < num; i++ {
+		d.load(f, args)
+	}
 	return d
 }
 
@@ -144,30 +226,61 @@ func (d *delegator) Genshin() {
 
 // Stop 暂停运行在goroutine上的委托,不会阻塞主协程
 func (d *delegator) Stop() {
+	if d.err != nil {
+		return
+	}
 	if d.typ {
-		d.stop <- 1
+		d.cs.stop <- 1
 	}
 }
 
 // Start 启动运行在goroutine上的委托,不会阻塞主协程
 func (d *delegator) Start() {
+	if d.err != nil {
+		return
+	}
 	if d.typ {
-		d.start <- 1
+		d.cs.start <- 1
 	}
 }
 
 // Wait 等待委托执行完毕,会阻塞主协程
 func (d *delegator) Wait() {
+	if d.err != nil {
+		return
+	}
 	if d.typ {
 		select {
-		case <-d.signal:
+		case <-d.cs.signal:
 		}
 	}
 }
 
+// Over 终止委托
 func (d *delegator) Over() {
+	if d.err != nil {
+		return
+	}
 	if d.typ {
-		d.over <- 1
+		//这里要加select,防止委托过早执行完毕导致主线程写入阻塞
+		select {
+		case d.cs.over <- 1:
+		default:
+		}
+	}
+}
+
+// Sleep 对stop和start的简单封装,因为委托走并发,所以不能保证立刻睡眠
+func (d *delegator) Sleep(duration time.Duration) {
+	if d.err != nil {
+		return
+	}
+	if d.typ {
+		go func() {
+			d.Stop()
+			time.Sleep(duration)
+			d.Start()
+		}()
 	}
 }
 
@@ -180,8 +293,8 @@ func (d *delegator) run() {
 	for i, f := range d.fs {
 		//select阻塞器,只有异步委托才会用上
 		select {
-		case <-d.stop:
-			<-d.start
+		case <-d.cs.stop:
+			<-d.cs.start
 		default:
 		}
 		//睡眠的优先级要在阻塞之后
@@ -193,15 +306,23 @@ func (d *delegator) run() {
 			returner.vals[i][j] = v2.Interface()
 		}
 	}
-	d.returns <- returner
+	d.cs.returns <- returner
 }
 
 // Run 执行委托,β型委托传参激活goroutine执行
-func (d *delegator) Run(params ...interface{}) {
+func (d *delegator) Run(params ...interface{}) error {
+	if d.err != nil {
+		return d.err
+	}
 	if params == nil || !d.typ {
 		//确定执行模式
 		switch d.ptn.carryPattern {
 		case Normal:
+			select {
+			case <-d.cs.over:
+				return nil
+			default:
+			}
 			d.run()
 		case Cycle:
 			//先确定ptn中的参数,
@@ -209,12 +330,12 @@ func (d *delegator) Run(params ...interface{}) {
 			if len(d.ptn.args) == 0 || d.ptn.args[0] == -1 {
 				for {
 					select {
-					case <-d.over:
-						break
+					case <-d.cs.over:
+						return nil
 					default:
 					}
 					d.run()
-					<-d.returns
+					<-d.cs.returns
 				}
 			} else {
 				count := 0
@@ -229,9 +350,14 @@ func (d *delegator) Run(params ...interface{}) {
 					count = 1
 				}
 				for i := 0; i < count; i++ {
+					select {
+					case <-d.cs.over:
+						return nil
+					default:
+					}
 					if i != 0 {
 						//这里要做下清空管道操作,防止死锁
-						<-d.returns
+						<-d.cs.returns
 					}
 					d.run()
 
@@ -245,6 +371,11 @@ func (d *delegator) Run(params ...interface{}) {
 			//确定执行模式
 			switch d.ptn.carryPattern {
 			case Normal:
+				select {
+				case <-d.cs.over:
+					return
+				default:
+				}
 				d.run()
 			case Cycle:
 				//先确定ptn中的参数,
@@ -253,13 +384,13 @@ func (d *delegator) Run(params ...interface{}) {
 
 					for {
 						select {
-						case <-d.over:
-							break
+						case <-d.cs.over:
+							return
 						default:
 						}
 						d.run()
 						//这里要做下清空管道操作,防止死锁
-						<-d.returns
+						<-d.cs.returns
 					}
 				} else {
 					count := 0
@@ -274,8 +405,15 @@ func (d *delegator) Run(params ...interface{}) {
 						count = 1
 					}
 					for i := 0; i < count; i++ {
+
+						select {
+						case <-d.cs.over:
+							return
+						default:
+						}
+
 						if i != 0 {
-							<-d.returns
+							<-d.cs.returns
 						}
 						d.run()
 						//这里要做下清空管道操作,防止死锁
@@ -283,16 +421,19 @@ func (d *delegator) Run(params ...interface{}) {
 				}
 			}
 
-			d.signal <- 1
+			d.cs.signal <- 1
 		}()
 	}
-
+	return d.err
 }
 
 // GetReturns 获取返回值管理单元,自带同步阻塞,会等到委托执行完毕
 func (d *delegator) GetReturns() Returner {
 	//先判断是否执行完毕,没执行完毕会等待执行完毕
-	tmp := <-d.returns
+	if d.err != nil {
+		return nil
+	}
+	tmp := <-d.cs.returns
 	return &tmp
 }
 
@@ -302,6 +443,9 @@ func (d *delegator) back() *delegator {
 
 // Join 连接另一个委托,叠加两个委托的函数队列
 func (d *delegator) Join(d2 Delegator) Delegator {
+	if d.err != nil {
+		return d
+	}
 	//要同步一下等待组的代办数和函数名
 	d.fs = append(d.fs, d2.back().fs...)
 	d.sleeper = append(d.sleeper, d2.back().sleeper...)
@@ -312,6 +456,9 @@ func (d *delegator) Join(d2 Delegator) Delegator {
 
 // SetTime 设置指定函数的睡眠时间,index参数为负数或超出委托成员长度则全部设置等长睡眠
 func (d *delegator) SetTime(t time.Duration, index int) Delegator {
+	if d.err != nil {
+		return d
+	}
 	//先初始化一下睡眠数组的切片
 	if index < 0 || index > len(d.names)-1 {
 		for i := range d.sleeper {
@@ -324,6 +471,9 @@ func (d *delegator) SetTime(t time.Duration, index int) Delegator {
 }
 
 func (d *delegator) SetPattern(pattern int, args ...interface{}) Delegator {
+	if d.err != nil {
+		return d
+	}
 	if pattern < 0 || pattern > max {
 		d.ptn.carryPattern = unEffective
 	} else {
